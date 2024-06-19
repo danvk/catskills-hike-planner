254e1252569437d8556553dd6bfcce40f78a1175
Dan Vanderkam <danvdk@gmail.com>
2024-06-19 14:47:42 -0400

    data update, accommodate elevation

--- README.md
+++ README.md
@@ -17,4 +17,25 @@ Local development:
       --header 'Content-Type: application/json' \
       --data-raw '{"peaks": ["H", "BD", "TC", "C", "Pl", "Su"]}'
 
+## Updating from computing-in-the-catskills repo
+
+The shared files are:
+
+- `graph.py`: no changes
+- `subset_cover.py`:
+  - [x] Orient the GeoJSON feature: back-port
+  - [x] Add `{'hike_index': i}` property: back-port
+  - [x] Drops `__main__` section: factor this out into a separate script
+- `peak_planner.py`: mostly independent, adds hard-coded code -> OSM ID table
+  - [ ] Derive this mapping from the data
+
+The shared data files are:
+
+- `data/hikes.json`
+- `data/network.geojson`
+
+The HTML parts of this repo are in catskills/map-src/
+The list of peaks and codes is hard-coded in `HikePlanner.tsx`.
+
+
 [planner]: https://danvk.org/catskills/map/planner
--- peak_planner.py
+++ peak_planner.py
@@ -10,42 +10,6 @@ import json
 from subset_cover import find_optimal_hikes_subset_cover
 
 
-PEAKS = [
-    ('S', 2426171552, 'Slide Mountain'),
-    ('H', 1938201532, 'Hunter Mountain'),
-    ('BD', 2473476912, 'Blackdome Mountain'),
-    ('BH', 2473476747, 'Blackhead Mountain'),
-    ('TC', 2473476927, 'Thomas Cole Mountain'),
-    ('We', 2955311547, 'West Kill Mountain'),
-    ('C', 2884119551, 'Cornell Mountain'),
-    ('Ta', 7292479776, 'Table Mountain'),
-    ('Pk', 2398015279, 'Peekamoose Mountain'),
-    ('Pl', 2882649917, 'Plateau Mountain'),
-    ('Su', 2882649730, 'Sugarloaf Mountain'),
-    ('W', 2884119672, 'Wittenberg Mountain'),
-    ('SW', 1938215682, 'Southwest Hunter'),
-    ('L', -6601, 'Lone Mountain'),
-    ('BL', 2897919022, 'Balsam Lake Mountain'),
-    ('P', 9147145385, 'Panther Mountain'),
-    ('BI', 357548762, 'Big Indian Mtn Mountain'),
-    ('Fr', 9953707705, 'Friday Mountain'),
-    ('Ru', 10033501291, 'Rusk Mountain'),
-    ('KHP', 9785950126, 'Kaaterskill High Peak'),
-    ('Tw', 7982977638, 'Twin Mountain'),
-    ('BC', 9953729846, 'Balsam Cap Mountain'),
-    ('Fi', 357559622, 'Fir Mountain'),
-    ('ND', 357574030, 'North Dome Mountain'),
-    ('B', 2845338212, 'Balsam Mountain'),
-    ('Bp', 212348771, 'Bearpen Mountain'),
-    ('E', 357557378, 'Eagle Mountain'),
-    ('IH', 7978185605, 'Indian Head Mountain'),
-    ('Sh', 10010091368, 'Sherrill Mountain'),
-    ('V', -5149, 'Vly Mountain'),
-    ('WHP', 2426236522, 'Windham High Peak'),
-    ('Ha', 357563196, 'Halcott Mountain'),
-    ('Ro', -6003, 'Rocky Mountain'),
-]
-
 
 MODES = [
     'unrestricted',
@@ -58,7 +22,13 @@ MODES = [
 
 def plan_hikes(peaks_to_hike: list[str], mode: str = 'unrestricted'):
     features = json.load(open('data/network+parking.geojson'))['features']
-    all_hikes: list[tuple[float, list[int]]] = json.load(open('data/hikes.json'))
+    all_hikes: list[tuple[float, float, list[int]]] = json.load(open('data/hikes.json'))
+
+    PEAKS = [
+        (f['properties']['code'], f['properties']['id'], f['properties']['name'])
+        for f in features
+        if f['properties'].get('type') == 'high-peak'
+    ]
 
     ha_code_to_osm_id = {ha_code: osm_id for ha_code, osm_id, _name in PEAKS}
     osm_ids = [ha_code_to_osm_id[ha_code] for ha_code in peaks_to_hike]
@@ -66,26 +36,36 @@ def plan_hikes(peaks_to_hike: list[str], mode: str = 'unrestricted'):
 
     osm_ids_set = set(osm_ids)
     relevant_hikes = [
-        h for h in all_hikes if any(peak_id in osm_ids_set for peak_id in h[1])
+        h for h in all_hikes if any(peak_id in osm_ids_set for peak_id in h[-1])
     ]
 
     if mode == 'unrestricted':
         hikes = relevant_hikes
     elif mode == 'loops-only':
-        hikes = [(d, nodes) for d, nodes in relevant_hikes if nodes[0] == nodes[-1]]
+        hikes = [
+            (d, ele, nodes) for d, ele, nodes in relevant_hikes if nodes[0] == nodes[-1]
+        ]
     elif mode == 'day-only':
         # TODO: parameterize the 21km
-        hikes = [(d, nodes) for d, nodes in relevant_hikes if d < 21]  # 21km = ~13 miles
+        hikes = [
+            (d, ele, nodes) for d, ele, nodes in relevant_hikes if d < 21
+        ]  # 21km = ~13 miles
     elif mode == 'day-loop-only':
-        hikes = [(d, nodes) for d, nodes in relevant_hikes if d < 21 and nodes[0] == nodes[-1]]
+        hikes = [
+            (d, ele, nodes)
+            for d, ele, nodes in relevant_hikes
+            if d < 21 and nodes[0] == nodes[-1]
+        ]
     elif mode == 'prefer-loop':
         # TODO: parameterize the 3.5km penalty
         hikes = [
-            (d + (0 if nodes[0] == nodes[-1] else 3.5), nodes, d) for d, nodes in relevant_hikes
+            (d + (0 if nodes[0] == nodes[-1] else 3.5), ele, nodes, d)
+            for d, ele, nodes in relevant_hikes
         ]
     elif mode == 'day-only-prefer-loop':
         hikes = [
-           (d + (0 if nodes[0] == nodes[-1] else 3.5), nodes, d) for d, nodes in relevant_hikes if d < 21
+           (d + (0 if nodes[0] == nodes[-1] else 3.5), ele, nodes, d)
+            for d, ele, nodes in relevant_hikes if d < 21
         ]
     else:
         raise ValueError(mode)
